27caa7df716405af51af249d3645f9006bb925b1
badicev <basakdilaracevik@hotmail.com>
2023-05-21 14:17:29 +0300

    creating an example database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow
+    QLineEdit, QPushButton, QMainWindow, QTableWidget
 from  PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow): #QMainWindow allows us to add a menu bar and a tool bar and a status bar.
 # #also we can add divisions. better for larger apps
@@ -18,6 +19,15 @@ class MainWindow(QMainWindow): #QMainWindow allows us to add a menu bar and a to
         about_action = QAction("Hakkında", self) #self will connect this QAction to the class
         help_menu_item.addAction(about_action)
         #about_action.setMenuRole(QAction.MenuRole.NoRole) #if help menu doesn't show up
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(8)
+        self.table.setHorizontalHeaderLabels(("ID", "İsim", "Grup", "Mentor", "Mentee Telefon", "Mentee E-posta", "Mentor Telefon", "Mentor E-posta")) #tuple
+        self.setCentralWidget(self.table)
+    def load_data(self):
+        connection = sqlite3.connect("mentorship.db")
+        result = connection.execute("SELECT * ")
+        self.table
 
 app = QApplication(sys.argv)
 nana = MainWindow()
